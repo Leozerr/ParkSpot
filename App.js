170071a6ba149a04688a1852493623864d07c6fc
Leozerr <patchanon-leo@hotmail.com>
2023-04-16 19:06:36 +0700

    Test

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
+/*import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
@@ -18,3 +18,54 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+*/
+import * as React from "react";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Button, StyleSheet } from "react-native";
+import MapView from "react-native-maps";
+
+const LoginButton = ({ onPress }) => {
+  return <Button title="Login" onPress={onPress} color="#007aff" />;
+};
+const RegisterButton = ({ onPress }) => {
+  return <Button title="Register" onPress={onPress} color="#007aff" />;
+};
+function ShowMap() {
+  return (
+    <MapView
+      style={{ flex: 1 }}
+      initialRegion={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    />
+  );
+}
+
+function HomeScreen() {
+  return (
+    <View>
+      <LoginButton />
+      <RegisterButton />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+      <ShowMap />
+    </NavigationContainer>
+  );
+}
+
+export default App;
