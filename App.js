7b04c5a0c205a74c44c5af3c567a1605080b1bc5
Daniel <62011127@kmitl.ac.th>
2023-04-18 01:05:57 +0700

    Sign Up Design

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import React, { FC, ReactElement, useState } from "react";
+import { View, Text, Pressable } from "react-native";
 import {
   NavigationContainer,
   useNavigationContainerRef,
   useNavigation,
 } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Button, StyleSheet, Image } from "react-native";
+import { Button, StyleSheet, Image, TextInput } from "react-native";
 import MapView, { Callout, Circle, Marker } from "react-native-maps";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
@@ -102,13 +102,103 @@ function LoginScreen() {
     </View>
   );
 }
-function RegisterScreen() {
+export const RegisterScreen = (props) => {
+  const { onPress, title = "Sign Up" } = props;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
   return (
-    <View>
-      <Text>Resgister Screen</Text>
+    <View
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#FFF",
+      }}
+    >
+      <Text style={styles.titleText}>Sign In to KMITL Parking</Text>
+      <Text style={styles.fieldText}>Username</Text>
+      <TextInput
+        style={styles.input}
+        value={username}
+        placeholder={"Username"}
+        onChangeText={(text) => setUsername(text)}
+        autoCapitalize={"none"}
+      />
+      <Text style={styles.fieldText}>Password</Text>
+      <TextInput
+        style={styles.input}
+        value={password}
+        placeholder={"Password"}
+        secureTextEntry
+        onChangeText={(text) => setPassword(text)}
+      />
+      <Text style={styles.fieldText}>Name</Text>
+      <TextInput
+        style={styles.input}
+        value={name}
+        placeholder={"Name"}
+        onChangeText={(text) => setName(text)}
+        autoCapitalize={"none"}
+      />
+      <Pressable style={styles.button} onPress={() => {}}>
+        <Text style={styles.text}>{title}</Text>
+      </Pressable>
     </View>
   );
-}
+};
+const styles = StyleSheet.create({
+  input: {
+    height: 30,
+    width: 300,
+    borderRadius: 16,
+    paddingLeft: 10,
+    backgroundColor: "#EFF3F8",
+  },
+
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 30,
+    paddingVertical: 10,
+    paddingHorizontal: 32,
+    borderRadius: 16,
+    width: 300,
+    elevation: 3,
+    backgroundColor: "#E35205",
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
+  },
+
+  titleText: {
+    fontSize: 20,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "#343434",
+    marginTop: -100,
+    marginBottom: 20,
+  },
+
+  fieldText: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "#343434",
+    marginTop: 5,
+    marginBottom: 10,
+  },
+});
 
 const Stack = createNativeStackNavigator();
 
