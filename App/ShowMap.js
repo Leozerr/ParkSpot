cc68f30b7a14b02daebd615f10abf826e6e91d28
Sprlnh <tuasabping@hotmail.com>
2023-05-04 20:27:43 +0700

    lkk

--- App/ShowMap.js
+++ App/ShowMap.js
@@ -1,4 +1,4 @@
-import React, { FC, ReactElement, useState } from "react";
+import React, { FC, ReactElement, useState, useEffect, useRef } from "react";
 import {
   View,
   Text,
@@ -6,6 +6,7 @@ import {
   Dimensions,
   Animated,
   TouchableOpacity,
+  Platform,
 } from "react-native";
 import {
   NavigationContainer,
@@ -23,17 +24,42 @@ import Fontisto from "react-native-vector-icons/Fontisto";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import { markers, mapDarkStyle, mapStandardStyle } from "../model/mapData.js";
 import { useTheme } from "@react-navigation/native";
+import * as Location from "expo-location";
 
 const { width, height } = Dimensions.get("window");
 const CARD_HEIGHT = 220;
 const CARD_WIDTH = width * 0.8;
 const SPACING_FOR_CARD_INSET = width * 0.1 - 10;
 
+// api key = "AIzaSyCC2ONx9Tr4pzoiW4mDGBa8yJYXjTZ8Tx0"
+
 export function ShowMap() {
-  const [pin, setPin] = React.useState({
-    latitude: 37.78825,
-    longitude: -122.4324,
-  });
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, [location]);
+
+  // console.log({ location });
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+  // console.log(location);
+  // console.log(location.coords.latitude + "", location.coords.longitude + "");
+  const mapRef = useRef(null);
 
   const initialMapState = {
     markers,
@@ -68,17 +94,31 @@ export function ShowMap() {
 
     _scrollView.current.scrollTo({ x: x, y: 0, animated: true });
   };
+
   const _map = React.useRef(null);
   const _scrollView = React.useRef(null);
   return (
     <View style={{ flex: 1, paddingTop: 50 }}>
       <MapView
         style={{ flex: 1 }}
+        showsUserLocation={true}
+        showsMyLocationButton={true}
+        ref={mapRef}
         initialRegion={{
           latitude: 13.727156,
           longitude: 100.77485,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
         }}
       >
+        {/* {location && (
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+          ></Marker>
+        )} */}
         {state.markers.map((marker, index) => {
           const scaleStyle = {
             transform: [
@@ -88,19 +128,21 @@ export function ShowMap() {
             ],
           };
           return (
-            <Marker
-              key={index}
-              coordinate={marker.coordinate}
-              onPress={(e) => onMarkerPress(e)}
-            >
-              <Animated.View style={[styles.markerWrap]}>
-                <Animated.Image
-                  source={require("../Image/marker1.png")}
-                  style={[styles.marker, scaleStyle]}
-                  resizeMode="cover"
-                />
-              </Animated.View>
-            </Marker>
+            <View>
+              <Marker
+                key={index}
+                coordinate={marker.coordinate}
+                onPress={(e) => onMarkerPress(e)}
+              >
+                <Animated.View style={[styles.markerWrap]}>
+                  <Animated.Image
+                    source={require("../Image/parkpin.png")}
+                    style={[styles.marker, scaleStyle]}
+                    resizeMode="cover"
+                  />
+                </Animated.View>
+              </Marker>
+            </View>
           );
         })}
       </MapView>
@@ -113,38 +155,6 @@ export function ShowMap() {
         />
         <Ionicons name="ios-search" size={29} />
       </View>
-
-      {/* <GooglePlacesAutocomplete
-        placeholder="Search"
-        fetchDetails={true}
-        GooglePlacesSearchQuery={{
-          rankby: "distance",
-        }}
-        onPress={(data, details = null) => {
-          console.log(data, details);
-          setRegion({
-            latitude: details.geometry.location.lat,
-            longitude: details.geometry.location.lng,
-          });
-        }}
-        query={{
-          key: "AIzaSyCC2ONx9Tr4pzoiW4mDGBa8yJYXjTZ8Tx0",
-          language: "th",
-          components: "country:th",
-          types: "establishment",
-          radius: 30000,
-          // location: `${region.latitude}, ${region.longitude}`,
-        }}
-        style={{
-          container: {
-            flex: 0,
-            position: "absolute",
-            width: "100%",
-            zIndex: 1,
-          },
-          listView: { BackgroundColor: "white" },
-        }}
-      /> */}
       <Animated.ScrollView
         ref={_scrollView}
         horizontal
